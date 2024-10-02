250baea26b543cba7a1becfc0c6e3f953c38b43a
Никита Антонов <Tyuiu.AntonovNS@outlook.com>
2024-10-02 15:38:49 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.AntonovNS.Sprint1.Task0.V29.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.AntonovNS.Sprint1.Task0.V29.Lib
+{
+    public class DataService : ISprint1Task0V29
+    {
+        public double Calculate()
+        {
+            return (2 * 9) + (7 * 2);
+        }
+    }
+}
---
+++ Tyuiu.AntonovNS.Sprint1.Task0.V29/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.AntonovNS.Sprint1.Task0.V29.Lib;
+
+namespace Tyuiu.AntonovNS.Sprint1.Task0.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Антонов Н.С. | ИСТНб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("#* Спринт #1                                                              *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнил: Антонов Никита Сергеевич |  ИСТНб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Условие:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение (2*9) + (7*2)           *");
+            Console.WriteLine("* и печатает результат на экран                                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* (2*9) + (7*2)                                                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate());
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
